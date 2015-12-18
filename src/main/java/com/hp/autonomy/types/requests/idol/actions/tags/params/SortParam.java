4de6a500cef19347c6da64b2e23e9346cf4dd5df
Alex Scown <alex.scown@hpe.com>
2015-12-18 11:45:32 +0000

    Add sort parameter for get query tag values. [rev: ivo.miller]

---
+++ src/main/java/com/hp/autonomy/types/requests/idol/actions/tags/params/SortParam.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright 2015 Hewlett-Packard Development Company, L.P.
+ * Licensed under the MIT License (the "License"); you may not use this file except in compliance with the License.
+ */
+
+package com.hp.autonomy.types.requests.idol.actions.tags.params;
+
+public enum SortParam {
+    Off,
+    Alphabetical,
+    Date,
+    DocumentCount,
+    NumberDecreasing,
+    NumberIncreasing,
+    ReverseAlphabetical,
+    ReverseDate,
+    ReverseDocumentCount;
+
+    public static SortParam fromValue(final String value) {
+        for (final SortParam param : values()) {
+            if (param.name().equalsIgnoreCase(value)) {
+                return param;
+            }
+        }
+
+        throw new IllegalArgumentException("Unknown parameter " + value);
+    }
+}
