aecfc6cddc11704fdba4edd5219c31af0dc6db3d
Alex Scown <alex.scown@hpe.com>
2016-02-22 18:24:02 +0000

    [CCUK-3692] Add additional actions and parameters

---
+++ src/main/java/com/hp/autonomy/types/requests/idol/actions/connector/ConnectorActions.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright 2015-2016 Hewlett-Packard Development Company, L.P.
+ * Licensed under the MIT License (the "License"); you may not use this file except in compliance with the License.
+ */
+
+package com.hp.autonomy.types.requests.idol.actions.connector;
+
+public enum ConnectorActions {
+    BackupServer,
+    GetUri,
+    PurgeDataStore,
+    QueueInfo,
+    RestoreServer,
+    StopFetch,
+    TaskList,
+    View
+}
---
+++ src/main/java/com/hp/autonomy/types/requests/idol/actions/connector/params/ConnectorViewParams.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright 2015-2016 Hewlett-Packard Development Company, L.P.
+ * Licensed under the MIT License (the "License"); you may not use this file except in compliance with the License.
+ */
+
+package com.hp.autonomy.types.requests.idol.actions.connector.params;
+
+public enum ConnectorViewParams {
+    Config,
+    // only works for Distributed Connector
+    Group,
+    Identifier,
+    NoACI
+}
