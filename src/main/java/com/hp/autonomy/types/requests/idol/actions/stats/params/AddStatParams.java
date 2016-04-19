65476dd9ce4eaad2d37d09af743ca997521cb0d3
Alex Scown <alex.scown@hpe.com>
2016-04-19 17:30:01 +0100

    Add statsserver parameters

---
+++ src/main/java/com/hp/autonomy/types/requests/idol/actions/stats/params/AddStatParams.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright 2015-2016 Hewlett-Packard Development Company, L.P.
+ * Licensed under the MIT License (the "License"); you may not use this file except in compliance with the License.
+ */
+
+package com.hp.autonomy.types.requests.idol.actions.stats.params;
+
+public enum AddStatParams {
+    Name,
+    AEqualStat,
+    ARangeStat,
+    BitANDStat,
+    NEqualStat,
+    NRangeStat,
+    DynamicField,
+    Field,
+    Offset,
+    Operation,
+    Period
+}
