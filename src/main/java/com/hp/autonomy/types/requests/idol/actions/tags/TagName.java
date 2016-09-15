a3351c655d5985408772f6c84a42131d2e539f44
Will Byrne <will.byrne@hpe.com>
2016-09-15 11:49:44 +0100

    [FIND-204] Update TagName to move logic
    Moved the TagName logic for extracting the name and path to
    HavenSearchComponents where it can be customised.
    
    [rev Ivo.Miller]

--- src/main/java/com/hp/autonomy/types/requests/idol/actions/tags/TagName.java
+++ src/main/java/com/hp/autonomy/types/requests/idol/actions/tags/TagName.java
@@ -5,35 +5,19 @@
 
 package com.hp.autonomy.types.requests.idol.actions.tags;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.Data;
 
 import java.io.Serializable;
 
 @SuppressWarnings({"WeakerAccess", "unused"})
-@Getter
-@NoArgsConstructor
-@EqualsAndHashCode
-public class TagName implements Serializable{
+@Data
+public class TagName implements Serializable {
     private static final long serialVersionUID = -6221132711228529797L;
-    private String id;
-    private String name;
+    private final String id;
+    private final String name;
 
-    @JsonCreator
-    public TagName(final String path) {
-        name = getFieldNameFromPath(path);
-        id = adjustFieldPath(path, name);
-    }
-
-    private String adjustFieldPath(final String fieldPath, final String fieldName) {
-        // Need an extra '/' to be able to query a field by its root path (since wildcards in Idol config file take the form */SOME_FIELD)
-        // However, for the special autn_date field which does not have a path, adding such a '/' would break the query
-        return fieldName.equals(fieldPath) || fieldPath.startsWith("/") ? fieldPath : '/' + fieldPath;
-    }
-
-    private String getFieldNameFromPath(final String value) {
-        return value.contains("/") ? value.substring(value.lastIndexOf('/') + 1) : value;
+    public TagName(final String name, final String path) {
+        this.name = name;
+        this.id = path;
     }
 }
